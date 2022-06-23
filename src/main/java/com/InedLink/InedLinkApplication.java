efe10225d38b97d6ca11b4edf36c714acdbe0f15
likealam <likealam@gmail.com>
2022-06-23 18:02:53 +0300

    Initial commit

---
+++ src/main/java/com/InedLink/InedLinkApplication.java
@@ -0,0 +1,13 @@
+package com.InedLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InedLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InedLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InedLink/InedLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InedLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InedLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
